d4ec9f734f9ed152226b6150a346c447a3f22068
Cleiton Leite <cleitonleite.melo@gmail.com>
2024-01-14 14:09:44 -0300

    Mapeando a entidade Produto

--- src/main/java/io/githob/cleiton/domain/entity/Cliente.java
+++ src/main/java/io/githob/cleiton/domain/entity/Cliente.java
@@ -9,7 +9,7 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
-   private Integer id;
+    private Integer id;
 
     @Column(name = "nome", length = 100)
     private String nome;
