babb9f77bd8ecd426496a4ad5d878d403ba82c4f
Cleiton Leite <cleitonleite.melo@gmail.com>
2024-01-31 15:26:49 -0300

    SWAGGER - Configurando o Security no Swagger

--- src/main/java/io/githob/cleiton/config/SwaggerConfig.java
+++ src/main/java/io/githob/cleiton/config/SwaggerConfig.java
@@ -1,15 +1,21 @@
 package io.githob.cleiton.config;
 
+import org.hibernate.mapping.Array;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -24,6 +30,8 @@ public class SwaggerConfig {
                         .basePackage("io.githob.cleiton.rest.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
                 .apiInfo(apiInfo());
     }
 
@@ -41,5 +49,28 @@ public class SwaggerConfig {
                 "Cleiton Leite",
                 "https://github.com/cleitonleite",
                 "cleitonleite.melo@gmail.com");
+    }
+
+    public ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext
+                .builder()
+                .securityReferences(defaulAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaulAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = authorizationScope;
+        SecurityReference reference = new SecurityReference("JWT", scopes);
+        List<SecurityReference> auths = new ArrayList<>();
+        auths.add(reference);
+        return auths;
     }
 }
